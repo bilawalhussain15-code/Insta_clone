081e651ccf0a858dd7d1320d452f890162f43569
bilawal <bilawalhussain1535@gmail.com>
2025-09-04 19:13:56 +0500

    Updated forms and urls

--- instaclone/core/forms.py
+++ instaclone/core/forms.py
@@ -1,17 +1,10 @@
-"""
-Forms for the Instaclone application.
-Handles user registration, profile editing, post creation, and comments.
-"""
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import CustomUser, Post, Comment
 
 
 class CustomUserCreationForm(UserCreationForm):
-    """
-    Custom user registration form with additional profile fields.
-    Extends Django's UserCreationForm.
-    """
+
     email = forms.EmailField(required=True)
     name = forms.CharField(max_length=150, required=False)
     bio = forms.CharField(widget=forms.Textarea, required=False)
@@ -26,7 +19,6 @@ class CustomUserCreationForm(UserCreationForm):
 
 
     def save(self, commit=True):
-        """Save the user with additional profile information"""
         user = super().save(commit=False)
         user.email = self.cleaned_data['email']
         user.name = self.cleaned_data['name']
@@ -42,21 +34,18 @@ class CustomUserCreationForm(UserCreationForm):
 
 
 class EditProfileForm(forms.ModelForm):
-    """Form for editing user profile information"""
     class Meta:
         model = CustomUser
         fields = ['username', 'name', 'email', 'bio', 'gender', 'profile_picture']
 
 
 class PostForm(forms.ModelForm):
-    """Form for creating new posts with image and caption"""
     class Meta:
         model = Post
         fields = ['image', 'caption']
 
 
 class CommentForm(forms.ModelForm):
-    """Form for adding comments to posts"""
     class Meta:
         model = Comment
         fields = ['text']
